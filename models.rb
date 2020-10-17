4817274749171afed5d815cc856f055c409b8bb4
勝　駿介 <>
2020-10-17 21:59:09 +0900

    heroku

--- models.rb
+++ models.rb
@@ -5,10 +5,6 @@ if development?
   ActiveRecord::Base.establish_connection('sqlite3:db/development.db')
 end
 
-class History < ActiveRecord::Base
-
-end
-
 class User < ActiveRecord::Base
   has_secure_password
 end
