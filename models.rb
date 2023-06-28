ee9909d80385ff1765b0e982545761fd30ed8825
Katsu-Shunsuke <64359144+Katsu-Shunsuke@users.noreply.github.com>
2023-06-29 02:50:55 +0900

    modify models.rb

--- models.rb
+++ models.rb
@@ -2,7 +2,11 @@ require 'bundler/setup'
 Bundler.require
 
 if development?
-  ActiveRecord::Base.establish_connection('sqlite3:db/development.db')
+  ActiveRecord::Base.establish_connection(
+    adapter: 'sqlite3',
+    database: 'db/development.db'
+  )
+  #ActiveRecord::Base.establish_connection('sqlite3:db/development.db')
 end
 
 class User < ActiveRecord::Base
